cf86f32841801019815f15d1c529550eb67b8e12
Fernanda Blos de Moraes <fernandabmads@gmail.com>
2026-08-18 10:59:38 -0300

    Commit inicia, com aulas 1 e 2

---
+++ Aula2/script.js
@@ -0,0 +1,29 @@
+usuario = {
+    nome : "",
+    peso : 0.0,
+    altura : 0.0,
+    calcularIMC : function(){
+        imc=this.peso/(this.altura*this.altura)
+        return imc
+    }
+}
+
+function calcular(){
+    nome = document.getElementById("txtUsuario").value
+    peso = document.getElementById("txtPeso").value
+    altura = document.getElementById("txtAltura").value
+
+
+    if (nome == "" || peso =="" || altura ==""){
+        alert("Nome, peso e altura devem ser preenchidos")
+    }else{
+        usuario.nome = nome
+        usuario.peso = peso.replace(",", ".")
+        usuario.altura = altura.replace(",", ".")
+        divIMC = document.getElementById("divIMC")
+        divIMC.innerHTML = "Nome: " + usuario.nome
+        divIMC.innerHTML += "<br>Peso: " + usuario.peso
+        divIMC.innerHTML += "<br>Altura: " + usuario.altura
+        divIMC.innerHTML += "<br>IMC: " + usuario.calcularIMC()
+    }
+}
